84714097d1c325bcc988c10a53a933f23d4f284a
Jeek11 <iandivinenniepes11@gmail.com>
2025-08-11 23:00:29 -0700

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -66,32 +66,9 @@ onMounted(() => {
 					placeholder="e.g. make a video"
 					v-model="input_content" />
 				
-				<h4>Pick a category</h4>
-				<div class="options">
+				
 
-					<label>
-						<input 
-							type="radio" 
-							name="category" 
-							id="category1" 
-							value="business"
-							v-model="input_category" />
-						<span class="bubble business"></span>
-						<div>Business</div>
-					</label>
-
-					<label>
-						<input 
-							type="radio" 
-							name="category" 
-							id="category2" 
-							value="personal"
-							v-model="input_category" />
-						<span class="bubble personal"></span>
-						<div>Personal</div>
-					</label>
-
-				</div>
+				
 
 				<input type="submit" value="Add todo" />
 			</form>
